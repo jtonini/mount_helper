e8301d532e7639c32e8c2f1753d0758b782e92fb
João Tonini <jtonini@richmond.edu>
2025-12-17 13:24:00 -0500

    add more functionality

--- backup-functions.sh
+++ backup-functions.sh
@@ -18,29 +18,55 @@ view_my_files_from() {
     else
         echo "Listing your backup files from $1..."
     fi
-    sudo /usr/local/bin/backup-mount-helper view "$1" "$USER" "$2"
+    sudo /usr/bin/backup-mount-helper view "$1" "$USER" "$2"
 }
 
 get_my_files_from() {
     if [ -z "$1" ]; then
-        echo "Usage: get_my_files_from <machine>"
+        echo "Usage: get_my_files_from <machine> [path]"
+        echo "Examples:"
+        echo "  get_my_files_from justin"
+        echo "  get_my_files_from justin Documents/projectA"
         echo "Available machines: $BACKUP_MACHINES"
         return 1
     fi
-    sudo /usr/local/bin/backup-mount-helper mount "$1" "$USER"
+    sudo /usr/bin/backup-mount-helper mount "$1" "$USER" "$2"
     if [ $? -eq 0 ]; then
-        echo "Your backup from $1 is now mounted at /backups/$1/$USER"
-        echo "When finished, run: release_my_files $1"
+        if [ -n "$2" ]; then
+            echo "Your backup from $1 is now mounted at /backups/$1/$USER/$2"
+            echo "When finished, run: release_my_files $1"
+        else
+            echo "Your backup from $1 is now mounted at /backups/$1/$USER"
+            echo "When finished, run: release_my_files $1"
+        fi
     fi
 }
 
 release_my_files() {
-    if [ -z "$1" ]; then
-        echo "Usage: release_my_files <machine>"
-        return 1
+    # Get list of mounts
+    if [ -n "$1" ]; then
+        MOUNTS=$(sudo /usr/bin/backup-mount-helper list "$1" "$USER")
+    else
+        MOUNTS=$(sudo /usr/bin/backup-mount-helper list "" "$USER")
     fi
-    sudo /usr/local/bin/backup-mount-helper umount "$1" "$USER"
-    if [ $? -eq 0 ]; then
-        echo "Backup from $1 has been unmounted."
+
+    if [ -z "$MOUNTS" ]; then
+        if [ -n "$1" ]; then
+            echo "No backups from $1 are currently mounted."
+        else
+            echo "No backups are currently mounted."
+        fi
+        return 0
+    fi
+
+    echo "The following backup(s) are mounted:"
+    echo "$MOUNTS" | while read -r m; do echo "  $m"; done
+    echo ""
+    read -p "Unmount these? [y/N] " confirm
+    if [[ "$confirm" =~ ^[Yy]$ ]]; then
+        sudo /usr/bin/backup-mount-helper umount "$1" "$USER"
+        echo "Backup(s) unmounted."
+    else
+        echo "Cancelled."
     fi
 }
